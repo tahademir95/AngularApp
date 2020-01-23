f5ae5da9b1f7f612d599539e18f028ee7465f661
tahademir95 <tahadmr.1995@gmail.com>
2020-01-23 19:16:10 +0300

    Two way binding

--- src/app/product.component.ts
+++ src/app/product.component.ts
@@ -5,7 +5,11 @@ import { Product } from './product.model';
 
 @Component({
     selector: "app",
-    templateUrl: "product.component.html",
+    template: `
+        <input [(ngModel)]="email" (keyup.enter)="onKeyUp()" /> 
+        <br>
+        <span> {{email}} </span>
+        `,
     styleUrls: [
         "product.component.css"
     ]
@@ -13,29 +17,10 @@ import { Product } from './product.model';
 export class ProductComponent {
     model: ProductRepository = new ProductRepository();
     
-    onSubmit($event){
-        $event.stopPropagation();       //butonlar div içinde olduğu için, butona tıklandığında div de tıklanmış oluyor, bu onu engelliyor
-        console.log('button is clicked');
-        console.log($event);
-    }
-    onDivClicked(){
-        console.log('div is clicked');
-    }
+    email = "email@email.com";
 
-    onKeyUp($event){
-
-        console.log($event.target.value);
-
-        if($event.keyCode === 13){
-            console.log("enter was pressed");
-        }
-        else if($event.keyCode === 32){
-            console.log("space was pressed");
-        }
-    }
-
-    onKeyUp2(email){
-        console.log(email);
+    onKeyUp(){
+        console.log(this.email);
     }
     
 }
