9ca9908ca3e2f94026abd9c5682e4c6748c4e2c5
tahademir95 <tahadmr.1995@gmail.com>
2020-02-05 19:28:13 +0300

    Listing produtcs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { ProductComponent } from './product.component';
 import { FormsModule } from '@angular/forms';
 import { SummaryPipe } from './summary.pipe';
 import { InputEmailDirective } from './input-email.directive';
+import { AdminProductsComponent } from './admin-products/admin-products.component';
 
 
 @NgModule({
   declarations: [
     ProductComponent,
     SummaryPipe,
-    InputEmailDirective
+    InputEmailDirective,
+    AdminProductsComponent
   ],
   imports: [
     BrowserModule,
